12595333fc161fc4116e54834dc06f97f26aaf55
Aysegul <karadan.ay7@gmail.com>
2024-07-22 22:40:16 +0300

    hanburger menu bg changed
    overlay hamburger bg changed

--- FE/components/Header.vue
+++ FE/components/Header.vue
@@ -22,7 +22,7 @@
   
       <div
         v-if="isMenuOpen"
-        class="flex flex-col lg:hidden top-11 absolute z-10 mx-auto left-0 right-0 border rounded-lg text-start border-gray-300 dark:border-gray-600 justify-center py-2 items-center  bg-transparent"
+        class="flex flex-col lg:hidden top-11 absolute z-20 mx-auto left-0 right-0 border rounded-lg text-start border-gray-300 dark:border-gray-600 justify-center py-2 items-center  bg-gray-900"
       >
         <div class="absolute top-0 w-full flex justify-end items-center">
           <NuxtLink
