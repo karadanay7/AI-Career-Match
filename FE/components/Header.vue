1023f38b1ebef628ac5b40ccab5dfd68d3182ea0
Aysegul <karadan.ay7@gmail.com>
2024-07-24 16:17:33 +0300

    Update Header.vue
    Hamburger icon button moved next to toogle button

--- FE/components/Header.vue
+++ FE/components/Header.vue
@@ -1,90 +1,94 @@
 <template>
+  <div
+    class="w-full flex items-center justify-between border-b border-primary dark:border-primary mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl bg-white dark:bg-gray-900"
+  >
+    <div>
+      <ULink to="/">
+        <h1>AICareerMatch</h1>
+      </ULink>
+    </div>
+    <div class="flex items-center gap-2 py-2 lg:hidden">
+      <UToggle
+        v-model="isDark"
+        on-icon="i-heroicons-moon"
+        off-icon="i-heroicons-sun"
+        size="lg"
+      />
+      <UButton @click="toggleMenu()">
+        <Icon
+          :name="isMenuOpen ? 'pajamas:close' : 'pajamas:hamburger'"
+          class="w-4 h-4 text-gray-300 dark:text-gray-900"
+        />
+      </UButton>
+    </div>
+
+    <div class="hidden lg:flex">
+      <UHorizontalNavigation :links="horizontalLinks" />
+    </div>
+
     <div
-      class="w-full flex items-center justify-between border-b  border-primary dark:border-primary  mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl bg-white dark:bg-gray-900"
+      v-if="isMenuOpen"
+      class="flex flex-col lg:hidden top-11 absolute z-20 mx-auto left-0 right-0 border rounded-lg text-start border-gray-300 dark:border-gray-600 justify-center py-2 items-center bg-gray-900"
     >
-      <div>
-        <ULink to="/">
-         <h1>AICareerMatch</h1>
-        </ULink>
-      </div>
-      <div class="py-2">
-        <UButton @click="toggleMenu()" class="lg:hidden">
+      <div class="absolute top-0 w-full flex justify-end items-center">
+        <NuxtLink
+          v-if="user"
+          @click="signOut()"
+          size="xs"
+          class="flex items-center w-full justify-end gap-1.5 text-sm text-primary p-2"
+        >
           <Icon
-            :name="isMenuOpen ? 'pajamas:close' : 'pajamas:hamburger'"
-            class="w-4 h-4 text-gray-300 dark:text-gray-900"
+            name="i-heroicons-arrow-right-start-on-rectangle-20-solid"
+            class="text-primary"
           />
-        </UButton>
-      </div>
-  
-      <div class="hidden lg:flex">
-        <UHorizontalNavigation :links="horizontalLinks" />
-      </div>
-  
-      <div
-        v-if="isMenuOpen"
-        class="flex flex-col lg:hidden top-11 absolute z-20 mx-auto left-0 right-0 border rounded-lg text-start border-gray-300 dark:border-gray-600 justify-center py-2 items-center  bg-gray-900"
-      >
-        <div class="absolute top-0 w-full flex justify-end items-center">
-          <NuxtLink
-            v-if="user"
-            @click="signOut()"
-            size="xs"
-            class="flex items-center w-full justify-end gap-1.5 text-sm text-primary p-2"
-          >
-            <Icon
-              name="i-heroicons-arrow-right-start-on-rectangle-20-solid"
-              class="text-primary"
-            />
-            Sign out
-          </NuxtLink>
-          <NuxtLink
-            v-if="!user"
-            @click="navigateAuth"
-            size="xs"
-            class="flex w-full items-center justify-end gap-1.5 text-sm text-primary p-2"
-          >
-            <Icon
-              name="i-heroicons-arrow-right-start-on-rectangle-20-solid"
-              class="text-primary"
-            />
-            Log in
-          </NuxtLink>
-        </div>
-  
-        <UVerticalNavigation :links="verticalLinks" />
-      </div>
-  
-      <div class="flex items-center justify-center gap-2">
-        <UButton
-        
+          Sign out
+        </NuxtLink>
+        <NuxtLink
           v-if="!user"
           @click="navigateAuth"
-          class=" hidden lg:flex "
-          size="sm"
-          icon="i-heroicons-arrow-right-end-on-rectangle-20-solid"
-          >Login</UButton
+          size="xs"
+          class="flex w-full items-center justify-end gap-1.5 text-sm text-primary p-2"
         >
-  
-        <UButton
-          class="hidden lg:flex w-24 "
-          v-else
-          @click="signOut()"
-          size="sm"
-           icon="i-heroicons-arrow-right-end-on-rectangle-20-solid"
-        >
-          
-          Logout
-        </UButton>
-        <UToggle
-          v-model="isDark"
-          on-icon="i-heroicons-moon"
-          off-icon="i-heroicons-sun"
-          size="lg"
-        />
+          <Icon
+            name="i-heroicons-arrow-right-start-on-rectangle-20-solid"
+            class="text-primary"
+          />
+          Log in
+        </NuxtLink>
       </div>
+
+      <UVerticalNavigation :links="verticalLinks" />
     </div>
-   
-  </template>
+
+    <div class="hidden lg:flex items-center justify-center gap-2">
+      <UButton
+        v-if="!user"
+        @click="navigateAuth"
+        class="hidden lg:flex"
+        size="sm"
+        icon="i-heroicons-arrow-right-end-on-rectangle-20-solid"
+      >
+        Login
+      </UButton>
+
+      <UButton
+        class="hidden lg:flex w-24"
+        v-else
+        @click="signOut()"
+        size="sm"
+        icon="i-heroicons-arrow-right-end-on-rectangle-20-solid"
+      >
+        Logout
+      </UButton>
+      <UToggle
+        v-model="isDark"
+        on-icon="i-heroicons-moon"
+        off-icon="i-heroicons-sun"
+        size="lg"
+      />
+    </div>
+  </div>
+</template>
   <script setup lang="ts">
   const supabase = useSupabaseClient();
 const user = useSupabaseUser();
