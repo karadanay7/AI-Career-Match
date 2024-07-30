88d75a812183f486740c9ebb5106a22c0e7d8577
bektascansel <cansel.bektas6447@gmail.com>
2024-07-30 15:53:38 +0300

    [Feat]: Create Infrastructure Layer and IJwtService, JwtManager

---
+++ BE/BE.Application/Features/UserAuth/Commands/UserEmployeeRegister/UserEmployeeRegisterCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using BE.Application.Features.UserAuth.Commands.UserRegister;
+using BE.Application.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Application.Features.UserAuth.Commands.UserEmployeeRegister
+{
+    public class UserEmployeeRegisterCommandHandler : IRequestHandler<UserEmployeeRegisterCommand, ResponseDto<RegisterDto>>
+    {
+
+
+
+
+
+        public Task<ResponseDto<RegisterDto>> Handle(UserEmployeeRegisterCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
