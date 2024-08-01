6811d3edac26fe779bfae1f99bddc38af3de6947
bektascansel <cansel.bektas6447@gmail.com>
2024-08-01 15:38:39 +0300

    [Feat]: Create ApplicationDbContext and IApplicationDbContext classes and Persistence folder

---
+++ BE/BE.Infrastructure/Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using BE.Application.Interfaces;
+using BE.Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Infrastructure.Persistence.Contexts
+{
+    public class ApplicationDbContext:IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ BE/BE.Infrastructure/Persistence/Contexts/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Infrastructure.Persistence.Contexts
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
