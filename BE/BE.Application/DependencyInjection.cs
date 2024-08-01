09bd483867004081ee2460f531cad64f37830edc
bektascansel <cansel.bektas6447@gmail.com>
2024-08-01 16:48:34 +0300

    [Feat]: Create ValidationBehavior and Add MediatR, DEpendecyInjections classes

---
+++ BE/BE.Application/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using BE.Application.Common.Behaviours;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+
+            });
+            return services;
+        }
+    }
+}
---
+++ BE/BE.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,50 @@
+﻿using BE.Application.Interfaces;
+using BE.Domain.Identity;
+using BE.Domain.Settings;
+using BE.Infrastructure.Persistence.Contexts;
+using BE.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+
+            services.AddScoped<IApplicationDbContext,ApplicationDbContext>(container=>container.GetRequiredService<ApplicationDbContext>());
+
+            services.AddDbContext<ApplicationDbContext>(options=>options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            services.Configure<JwtSettings>(jwtSettings => configuration.GetSection("JwtSettings").Bind(jwtSettings));
+
+
+            services.AddIdentity<User, Role>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+
+                options.User.RequireUniqueEmail = true;
+
+            })
+               .AddEntityFrameworkStores<ApplicationDbContext>()
+               .AddDefaultTokenProviders();
+
+            services.AddScoped<IJwtService, JwtManager>();
+            services.AddScoped<IIdentityService, IdentityManager>();
+
+            return services;
+        }
+    }
+}
