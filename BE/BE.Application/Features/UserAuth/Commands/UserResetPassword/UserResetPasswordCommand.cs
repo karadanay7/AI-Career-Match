e1f97c029385195166b188c017ea7dc3c667cd0c
bektascansel <cansel.bektas6447@gmail.com>
2024-08-09 14:43:34 +0300

    [Feat]: Create UserResetPassword CQRS features and Add new function to EmailService

---
+++ BE/BE.Application/Features/UserAuth/Commands/UserResetPassword/UserResetPasswordCommand.cs
@@ -0,0 +1,18 @@
+﻿using BE.Application.Common.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.Application.Features.UserAuth.Commands.UserResetPassword
+{
+    public class UserResetPasswordCommand:IRequest<ResponseDto<bool>>
+    {
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
